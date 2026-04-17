e165a57916065f846023ce142a4f1128ea2179bb
piyush-chavan05 <chavanpb05@gmail.com>
2026-04-17 22:53:26 +0530

    fix: prevent KeyError in expense formatting and add indexed display

--- app/expense.py
+++ app/expense.py
@@ -1,4 +1,6 @@
-def create_expense(amount, category, description):
+from storage import load_expenses
+
+def create_expense(amount, category, description, id=None):
     categories = {
         '1': 'Food',
         '2': 'Travel',
@@ -7,13 +9,22 @@ def create_expense(amount, category, description):
         '5': 'Personal',
         '6': 'Other'
     }
+    id = len(load_expenses()) + 1
     return{
+        'id': id,
         'amount': amount,
         'category': categories[category],
         'description': description}
     
 def format_expense(expense):
-    return print(f"₹{expense['amount']}/- | {expense['category']} | {expense['description']}")
+    return print(f'''
+    Index: {expense.get('id', 'N/A')} 
+          ₹{expense.get('amount', 0)}/-      |      {expense.get('category', 'Unknown')}
+    ---------------------------------------  
+    Description:
+    {expense.get('description', 'No description')}
+    ---------------------------------------
+    ''')
 
 def format_expenses_list(expenses):
     formatted_listt = []
